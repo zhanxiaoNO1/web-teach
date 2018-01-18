7fd48a558890ddbb8a3e243143f250af22381fc8
23-xujian <1210712999@qq.com>
2018-01-18 21:39:53 +0800

    0118

---
+++ 练习/0118/js/controller.js
@@ -0,0 +1,4 @@
+angular.module("my.controller",[])
+.controller("mycontroller",function($scope,content){
+	$scope.arr=content.arr
+})
---
+++ 练习/0118/js/service.js
@@ -0,0 +1,28 @@
+angular.module("my.service",[])
+.factory("content",function(){
+	return {
+		arr : [[{
+				"img" :"img/Ncontact1.jpg",
+				"name": "38客服",
+				"price": 20
+			},
+			{
+				"img" :"img/Ncontact1.jpg",
+				"name": "38客服",
+				"price": 20
+			}],
+			[{
+				"img" :"img/Ncontact1.jpg",
+				"name": "38客服",
+				"price": 20
+			},
+			{
+				"img" :"img/Ncontact1.jpg",
+				"name": "38客服",
+				"price": 20
+			}
+		]]
+	}
+		
+})
+
