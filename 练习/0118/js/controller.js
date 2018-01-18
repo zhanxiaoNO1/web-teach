8d5fc01d1fd4c5427d8417432eaddb4ae878668c
19-houshiqi <2363436064@qq.com>
2018-01-18 21:51:40 +0800

    练习

---
+++ 练习/0118/js/controller.js
@@ -0,0 +1,19 @@
+app.controller("myCtrl", function($scope) {
+			$scope.names = [{
+				pic: "img/1.jpg",
+				name: "商品1",
+				price: 11
+			}, {
+				pic: "img/1.jpg",
+				name: "商品2",
+				price: 22
+			}, {
+				pic: "img/1.jpg",
+				name: "商品3",
+				price: 33
+			}, {
+				pic: "img/1.jpg",
+				name: "商品4",
+				price: 44
+			}]
+		})
