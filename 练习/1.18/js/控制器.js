d8d98057a9970e10cf20528f92190214660c7ec7
05-chensai <782444245@qq.com>
2018-01-18 21:23:30 +0800

    练习

---
+++ 练习/1.18/js/控制器.js
@@ -0,0 +1,7 @@
+myApp.controller('firstController',  function($scope,Data){
+    $scope.x = {name: "china"};
+    
+});
+ myApp.controller("secondController",function($scope,Data){
+ 	$scope.y = {name: "china"};
+ });
