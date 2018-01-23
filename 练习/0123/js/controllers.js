83bf7e962cf9dba4cfb75be9a84e71c510828b3f
xqr <1063229235@qq.com>
2018-01-23 22:52:54 +0800

    0123作业日报

---
+++ 练习/0123/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($scope,$http,$rootScope) {
+	$http.post($rootScope.URLAdmin+"/Handler/OfflineCourseHandler.ashx?action=indexshow")
+	.then(function(res){
+		console.log(res.data);
+		$scope.bannerList = res.data.data.bannerList;
+	})
+})
+
+.controller('LessonlistCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('MycourseCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('PersonalCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
