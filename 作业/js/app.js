208845bb164338edd6e776f91edf3f89842a673d
07-chentianju <1361153656@qq.com>
2018-01-24 22:05:25 +0800

    2018.1.24日报  作业

--- 作业/js/app.js
+++ 作业/js/app.js
@@ -1,9 +1,105 @@
-state('slider', {
- url: '/slider',
- views: {
- '': {
-  templateUrl: 'templates/slider.html',
-  controller: 'SliderCtrl'
- }
- }
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // abstract state for the tabs directive
+  //.state是路由设置方法
+ /* 第一个参数是该路由的状态名  自己起
+  第二个参数是一个配置对象
+  配置对象中{
+  	url设置该路由跳转路径
+  	之后的路由跳转可通过状态名和路径两种方式跳转
+  	abstract抽象该路由,作为根路由
+  }*/
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+    /*设置模板页面路径*/
+  })
+
+  // Each tab has its own nav history stack:
+	//views是设置该视图加载到哪个视图容器中,类型为对象
+	//其中，有一个属性为视图容器名
+  .state('tab.home', {
+    url: '/home',
+    views: {
+      'tab-home': {
+        templateUrl: 'templates/tab-home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+	
+	/*.state('tab.asd',{
+		url:'/asd',
+		views:{
+			'tab-home':{
+				templateUrl:'templates/tab-asd.html',
+				controller:'AsdCtrl'
+			}
+		}
+	})*/
+	
+  .state('tab.lessonlist', {
+      url: '/lessonlist',
+      views: {
+        'tab-lessonlist': {
+          templateUrl: 'templates/tab-lessonlist.html',
+          controller: 'LessonlistCtrl'
+        }
+      }
+    })
+    .state('tab.mycourse', {
+      url: '/mycourse',
+      views: {
+        'tab-mycourse': {
+          templateUrl: 'templates/tab-mycourse.html',
+          controller: 'MycourseCtrl'
+        }
+      }
+    })
+
+  .state('tab.personal', {
+    url: '/personal',
+    views: {
+      'tab-personal': {
+        templateUrl: 'templates/tab-personal.html',
+        controller: 'PersonalCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/home');
+
+});
--- 作业/js/controllers.js
+++ 作业/js/controllers.js
@@ -8,30 +8,173 @@ angular.module('starter.controllers', [])
 		console.log($scope.lunbopics)
 		$ionicSlideBoxDelegate.$getByHandle("slideimgs").update();
 		$ionicSlideBoxDelegate.$getByHandle("slideimgs").loop("true");
+		
+		//好评榜数据
+		$scope.homeGoodlistRows = [
+		[res.data.data.goodList[0],res.data.data.goodList[1]],
+		[res.data.data.goodList[2],res.data.data.goodList[3]]
+		];
+		
+		//最新课程数据
+		$scope.homeNewLists = [
+		[res.data.data.newList[0],res.data.data.newList[1]],
+		[res.data.data.newList[2],res.data.data.newList[3]]
+		];
+		
+		//猜你喜欢数据
+		$scope.homechooseLists = res.data.data.chooseList;
 	})
 })
 
 
 /*.controller('AsdCtrl', function($scope) {})*/
 
-.controller('LessonlistCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('LessonlistCtrl', function($scope,$http,$rootScope) {
+	$http.post($rootScope.URLAdmin+'/Handler/OfflineCourseHandler.ashx?action=getcategory')
+	.then(function(res){
+		console.log(res.data)
+		$scope.lessons=[
+		[res.data.data[0],res.data.data[1],res.data.data[2],res.data.data[3]],
+		[res.data.data[4],res.data.data[5],res.data.data[6],res.data.data[7]],
+		[res.data.data[8],res.data.data[9],res.data.data[10],res.data.data[11]],
+		[res.data.data[12],res.data.data[13],res.data.data[14],res.data.data[15]],
+		[res.data.data[16],res.data.data[17],res.data.data[18],res.data.data[19]],
+		[res.data.data[20],res.data.data[21]]
+		];
+		
+	})
+  	var arr1=["全部","免费","收费"]
+		$scope.datas=arr1;
+		
+		$scope.myoj = {}
+		$scope.myojj={}
+		$scope.aa = function(){
+				$rootScope.cccc = !$rootScope.cccc
+				$rootScope.dddd = false
+				$scope.myobj={
+					"color":"green"
+				}
+				$scope.myojj={}
+			};
+		 
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+		$scope.bb = function() {
+				$rootScope.dddd = !$rootScope.dddd
+				$rootScope.cccc = false
+				$scope.myojj={
+					"color":"green"
+				}
+				$scope.myobj={}
+			};	
+			
+			
+	var arr=[
+			{
+				"img":"img/01.jpg",
+				"name":"轻轻",
+				"price":18
+			},
+			{
+				"img":"img/02.jpg",
+				"name":"娃娃",
+				"price":17
+			},
+			{
+				"img":"img/03.jpg",
+				"name":"娥娥",
+				"price":16
+			},
+			{
+				"img":"img/04.jpg",
+				"name":"然然",
+				"price":19
+			},{
+				"img":"img/05.jpg",
+				"name":"婷婷",
+				"price":20
+			},{
+				"img":"img/06.jpg",
+				"name":"uu",
+				"price":23
+			},{
+				"img":"img/07.jpg",
+				"name":"飘飘",
+				"price":21
+			}
+		];
+	$scope.dataq=arr;
+	})
+  
+
 
 .controller('MycourseCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+		$scope.chat = Chats.get($stateParams.chatId);
+  
+		  $scope.data= {
+		    showDelete: false
+		  };
+		  
+		  $scope.edit = function(item) {
+		    alert('Edit Item: ' + item.id 
+
+);
+		  };
+		  $scope.share = function(item) {
+		    alert('Share Item: ' + item.id 
+
+);
+		  };
+		  
+		  $scope.moveItem = function(item, fromIndex, toIndex) {
+		    $scope.items.splice(fromIndex, 1);
+		    $scope.items.splice(toIndex, 0, item);
+		  };
+		  
+		  $scope.onItemDelete = function(item) {
+		    $scope.items.splice($scope.items.indexOf(item), 1);
+		  };
+  
+	  $scope.items = [
+	    {"img":"img/01.jpg",
+		 "name":"轻轻",
+		 "price":18 },
+	    { "img":"img/02.jpg",
+				"name":"娃娃",
+				"price":17 },
+	    { "img":"img/03.jpg",
+				"name":"娥娥",
+				"price":16 },
+	    { "img":"img/04.jpg",
+				"name":"然然",
+				"price":19 },
+	    { "img":"img/05.jpg",
+				"name":"婷婷",
+				"price":20 },
+	    { "img":"img/06.jpg",
+				"name":"uu",
+				"price":23 },
+	    { "img":"img/07.jpg",
+				"name":"飘飘",
+				"price":21 },
+	    { "img":"img/01.jpg",
+		 		"name":"轻轻",
+		 		"price":18 },
+	    { "img":"img/03.jpg",
+				"name":"娥娥",
+				"price":16 },
+	    { "img":"img/05.jpg",
+				"name":"婷婷",
+				"price":20 },
+	    { "img":"img/06.jpg",
+				"name":"uu",
+				"price":23 },
+	    { "img":"img/07.jpg",
+				"name":"飘飘",
+				"price":21 }
+	 
+  		];
+  		
+	})
 
 .controller('PersonalCtrl', function($scope) {
   $scope.settings = {
