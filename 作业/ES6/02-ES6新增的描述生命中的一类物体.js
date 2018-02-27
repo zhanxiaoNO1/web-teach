432eab763f1bf233d745aa1aedeed6c778c54cf2
07-chentianju <1361153656@qq.com>
2018-02-27 23:38:02 +0800

    作业

---
+++ 作业/ES6/02-ES6新增的描述生命中的一类物体.js
@@ -0,0 +1,10 @@
+var Light = /** @class */ (function () {
+    function Light() {
+    }
+    return Light;
+}());
+var duoduo = new Light();
+duoduo.id = 19951111;
+duoduo.model = '超大';
+duoduo.prise = 200;
+console.log('多多的灯型号：' + duoduo.model);
---
+++ 作业/ES6/02-ES6新增的描述生命中的一类物体.ts
@@ -0,0 +1,11 @@
+class  Light{
+    id:number;
+    model:string;
+    prise:number;
+}
+
+let duoduo = new  Light();
+duoduo.id = 19951111;
+duoduo.model = '超大';
+duoduo.prise = 200;
+console.log('多多的灯型号：'+duoduo.model);
